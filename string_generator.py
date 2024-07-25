7eec96fad332dc7437875fadcd394b66dcf06135
ZealBots07 <zealanime@protonmail.com>
2024-07-25 16:42:23 +0530

    Update string_generator.py

--- string_generator.py
+++ string_generator.py
@@ -7,7 +7,7 @@ Login using your Telegram account
 Click on API Development Tools
 Create a new application, by entering the required details"""
 )
-APP_ID = int(input("Enter APP ID here: "))
-API_HASH = input("Enter API HASH here: ")
+APP_ID = int(input("28408158: "))
+API_HASH = input("ddda6703b42211276b28252aca309a2d: ")
 with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
     print(client.session.save())
